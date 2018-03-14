551507c1ce95143486250e36e7b99a5cdffbe0b3
Ian Brown <bigianb@gmail.com>
2018-03-14 00:27:18 +0000

    travis build

--- tests/emu/attotime.cpp
+++ tests/emu/attotime.cpp
@@ -7,10 +7,10 @@ BOOST_AUTO_TEST_CASE(test_string_formatting)
 {
    attotime value = attotime::from_seconds(1);
    BOOST_CHECK(value.as_attoseconds() == 1000000000000000000);
-   BOOST_TEST(value.as_string(0) == "1");
-   BOOST_TEST(value.as_string(4) == "1.0000");
-   BOOST_TEST(value.as_string(10) == "1.0000000000");
-   BOOST_TEST(value.as_string(15) == "1.000000000000000");
+   BOOST_CHECK(value.as_string(0) == "1");
+   BOOST_CHECK(value.as_string(4) == "1.0000");
+   BOOST_CHECK(value.as_string(10) == "1.0000000000");
+   BOOST_CHECK(value.as_string(15) == "1.000000000000000");
 }
 
 BOOST_AUTO_TEST_CASE(test_never)
