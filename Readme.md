558bde058745c1b706a7e65ea86f46e83d539f8a
Ian Brown <bigianb@gmail.com>
2018-03-14 17:14:03 +0000

    attottime unit tests

--- Readme.md
+++ Readme.md
@@ -1,3 +1,12 @@
-[![Build Status](https://travis-ci.org/bigianb/remame.svg?branch=master)](https://travis-ci.org/bigianb/remame)
+[![Build Status](https://travis-ci.org/bigianb/remame.svg?branch=master)](https://travis-ci.org/bigianb/remame) [![Coverage Status](https://coveralls.io/repos/github/bigianb/remame/badge.svg?branch=master)](https://coveralls.io/github/bigianb/remame?branch=master)
 
-[![Coverage Status](https://coveralls.io/repos/github/bigianb/remame/badge.svg?branch=master)](https://coveralls.io/github/bigianb/remame?branch=master)
+# What is this?
+
+TL;DR There's nothing of interest to see here.
+
+I'm slowly re-building a subset of mame with 2 mail goals:
+  1. To understand the mame codebase better
+  2. To gain some experience in CI tools such as Travis
+
+Please feel free to look around, make a clone etc. Don't however expect the codebase to work or do anything useful.
+
--- tests/emu/attotime.cpp
+++ tests/emu/attotime.cpp
@@ -11,10 +11,40 @@ BOOST_AUTO_TEST_CASE(test_string_formatting)
    BOOST_CHECK_EQUAL(value.as_string(4), "1.0000");
    BOOST_CHECK_EQUAL(value.as_string(10), "1.0000000000");
    BOOST_CHECK_EQUAL(value.as_string(15), "1.000000000000000");
+   BOOST_CHECK_EQUAL(attotime::never.as_string(5), "(never)");
 }
 
 BOOST_AUTO_TEST_CASE(test_never)
 {
 	attotime value = attotime::from_seconds(1);
 	BOOST_CHECK(!value.is_never());
+}
+
+BOOST_AUTO_TEST_CASE(test_mul)
+{
+	attotime twoSecs = attotime::from_seconds(2);
+    attotime threeSecs = attotime::from_seconds(3);
+    attotime val = twoSecs * 3;
+	BOOST_CHECK(val == attotime::from_seconds(6));
+
+    val = attotime::never * 3;
+	BOOST_CHECK(val == attotime::never);
+
+    val = twoSecs * 0;
+	BOOST_CHECK(val == attotime::zero);
+}
+
+BOOST_AUTO_TEST_CASE(test_div)
+{
+	attotime twoSecs = attotime::from_seconds(2);
+    attotime sixSecs = attotime::from_seconds(6);
+    attotime val = twoSecs / 3;
+	BOOST_CHECK(val == twoSecs);
+
+    val = attotime::never / 3;
+	BOOST_CHECK(val == attotime::never);
+
+    // divide by zero is ignored
+    val = twoSecs / 0;
+	BOOST_CHECK(val == twoSecs);
 }
